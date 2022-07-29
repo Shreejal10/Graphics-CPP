9a9bb48f24d37415e01c181cb63f8f32a80fabaf
Shreejal Dhungana <dshreejal@gmail.com>
2022-07-29 17:09:10 +0545

    DDA algorithm using slope

--- DDA-ALGORITHM/dda-algorithm.cpp
+++ DDA-ALGORITHM/dda-algorithm.cpp
@@ -8,33 +8,31 @@ using namespace std;
 int main(){
 	int gd=DETECT, gm;
 	initgraph (&gd, &gm,"");
-//	initwindow(800,800);
-	float x0,y0,x1,y1,x,y,dx,dy,steps;
+	float x0,y0,x1,y1,x,y,dx,dy;
 	cout<<"Enter value of x0 and y0: ";
 	cin>>x0>>y0;
 	cout<<"Enter value of x1 and y1: ";
 	cin>>x1>>y1;
-	
 	dx=x1-x0;
 	dy=y1-y0;
-	
-	if(dx>dy){
-		steps=abs(dx);
-	}
-	else{
-		steps=abs(dy);
-	}
-	
-	float Xincrement=dx/steps;
-	float Yincrement=dy/steps;
-	
-	x=x1;
-	y=y1;
-	putpixel (round(x),round(y),WHITE);
-	for(int i=1; i<=steps; i++){
-		x=x+Xincrement;
-		y=y+Yincrement;
-		putpixel (round(x),round(y),WHITE);
+	x=x0;
+	y=y0;
+	float m = dy/dx;
+    putpixel (x,y,WHITE);
+	while(x!=x1 && y!=y1){
+		if(m<1){
+			x=x+1;
+			y=y+m;
+		}
+		else if(m>1){
+			x=x+(1/m);
+			y=y+1;
+		}
+		else{
+			x=x+1;
+			y=y+m;
+		}
+		putpixel (x,y,WHITE);
 		delay(100);
 	}
 	getch();
